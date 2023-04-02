36e2eda1f08c0d32bf209e1f9a8f56d95ce78151
Socram1012 <marcos.medina@usm.cl>
2023-04-02 18:08:33 -0400

    Initial commit

---
+++ src/main/java/com/proyecto/cens/CensApplication.java
@@ -0,0 +1,13 @@
+package com.proyecto.cens;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CensApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CensApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/proyecto/cens/CensApplicationTests.java
@@ -0,0 +1,13 @@
+package com.proyecto.cens;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CensApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
