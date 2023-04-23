3975edd165fe8092a090409c805510378d9a6243
Socram1012 <marcos.medina@usm.cl>
2023-04-23 03:26:15 -0400

    Controllers, utils

--- src/main/java/com/proyecto/cens/controllers/EmpresaController.java
+++ src/main/java/com/proyecto/cens/controllers/EmpresaController.java
@@ -9,7 +9,7 @@ import org.springframework.web.bind.annotation.RestController;
 import java.util.List;
 
 @RestController
-public class EmpresaControllers {
+public class EmpresaController {
     @Autowired
     private EmpresaDao empresaDao;
 
--- src/main/java/com/proyecto/cens/dao/UsuarioDao.java
+++ src/main/java/com/proyecto/cens/dao/UsuarioDao.java
@@ -1,4 +1,15 @@
 package com.proyecto.cens.dao;
 
+import com.proyecto.cens.models.Usuario;
+
+import java.util.List;
+
 public interface UsuarioDao {
+    List<Usuario> getUsuarios();
+
+    void eliminar(Long id);
+
+    void registrar(Usuario usuario);
+
+    Usuario obtenerUsuarioPorCredenciales(Usuario usuario);
 }
